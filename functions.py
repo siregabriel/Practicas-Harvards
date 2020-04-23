4abd4737c7a9da4ebd0d99039928ac65bc8c7627
Gabriel Rosales <gabriel@gabrielrosales.org>
2020-04-23 13:05:23 -0500

    funciones

---
+++ functions.py
@@ -0,0 +1,7 @@
+def square(x):
+    return x * x
+
+for i in range(10):
+    print("{} squared is {}" .format(i, square(i)))
+
+#esta sencuencia se multiplica en si mismo hasta el numero 9
--- loops0.py
+++ loops0.py
@@ -1,3 +1,19 @@
-names = ["Alice", "Bob", "Charlie"]
+
+titulo = "El titulo de esta sección es: -Dictionaries-"
+contenido = "Revisar documentación por favor"
+miembros = "Familia:"
+
+print(titulo)
+print(contenido)
+
+print(miembros)
+names = ["Gaby", "Gabriel", "Liam"]
 for name in names:
    print(name)
+
+ages = {"Gaby":50, "Gabriel":40, "Liam":6}
+ages["Gaby"] -= 9
+ages["Gabriel"] += 1
+ages["Liam"] += 2
+
+print(ages)
