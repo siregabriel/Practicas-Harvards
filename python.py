06791a2d0ed1a99f2d796d6928b3f87514a97e92
Gabriel Rosales <gabriel@gabrielrosales.org>
2020-04-23 11:57:58 -0500

    practicas de Python

--- python.py
+++ python.py
@@ -1,1 +1,27 @@
-print ("Hello")
+#name = input() <<<VARIABLE
+#print("GabrielR Systems 3.2") <<<SOLO POR ADORNARME
+#print(f"Hola, {name}")<<<FUNCION
+
+i = 28
+print(f"f is {i}")
+f = 2.8
+print(f"f is {f}")
+b = True
+print(f"b is {b}")
+n = None 
+print(f"n is {n}")
+
+x = 27
+if x > 29:
+    print("x is positive")
+elif x < 28:
+    print("x is negative")
+else:
+    print("x is zero")
+
+name ="Alice"
+name[2]
+#esto de arriba cuenta e imprime la SEGUNDA LETRA de "Alice"
+coordinates = (10, 19.8)
+coordinates [2]
+#esto de arriba cuenta e imprime la SEGUNDA COORDENADA
